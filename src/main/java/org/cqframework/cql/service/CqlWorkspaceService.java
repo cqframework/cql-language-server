691015baeaae8f206001824952b5320bcb571d8b
Jonathan Percival <jonathan.i.percival@gmail.com>
2019-06-10 23:55:36 -0600

    Start of execution support

--- src/main/java/org/cqframework/cql/service/CqlWorkspaceService.java
+++ src/main/java/org/cqframework/cql/service/CqlWorkspaceService.java
@@ -1,5 +1,6 @@
 package org.cqframework.cql.service;
 
+import java.io.ByteArrayInputStream;
 import java.net.URI;
 import java.util.Collection;
 import java.util.HashMap;
@@ -13,6 +14,8 @@ import com.google.gson.JsonElement;
 
 import org.cqframework.cql.CqlLanguageServer;
 import org.cqframework.cql.cql2elm.CqlTranslator;
+import org.cqframework.cql.cql2elm.model.TranslatedLibrary;
+import org.cqframework.cql.elm.execution.Library;
 import org.eclipse.lsp4j.DidChangeConfigurationParams;
 import org.eclipse.lsp4j.DidChangeWatchedFilesParams;
 import org.eclipse.lsp4j.DidChangeWorkspaceFoldersParams;
@@ -22,6 +25,9 @@ import org.eclipse.lsp4j.MessageType;
 import org.eclipse.lsp4j.WorkspaceFolder;
 import org.eclipse.lsp4j.services.LanguageClient;
 import org.eclipse.lsp4j.services.WorkspaceService;
+import org.opencds.cqf.cql.execution.Context;
+import org.opencds.cqf.cql.execution.CqlLibraryReader;
+import org.opencds.cqf.cql.file.fhir.FileBasedFhirProvider;
 
 public class CqlWorkspaceService implements WorkspaceService {
     private static final Logger LOG = Logger.getLogger("main");
@@ -43,31 +49,16 @@ public class CqlWorkspaceService implements WorkspaceService {
 
     @Override
     public CompletableFuture<Object> executeCommand(ExecuteCommandParams params) {
-
-        try 
-        {
             String command = params.getCommand();
-            // There's currently not a "show text file" or similar command in the LSP spec,
-            // So it's not client agnostic. The client has to know that the result of this command
-            // is XML and display it accordingly.
-            if (command.equals("Other.ViewXML")) {
-                String uriString = ((JsonElement)params.getArguments().get(0)).getAsString();
-                URI uri = new URI(uriString);
-                Optional<String> content = ((CqlTextDocumentService)this.server.getTextDocumentService()).activeContent(uri);
-                if (content.isPresent()) {
-                    CqlTranslator translator = this.server.getTranslationManager().translate(uri, content.get());
-                    return CompletableFuture.completedFuture(translator.toXml());
-                }
+            switch (command) {
+                case "Other.ViewXML":
+                    return this.viewXml(params);
+                case "Other.ExecuteCql":
+                    return this.executeCql(params);
+                default:
+                    this.client.join().showMessage(new MessageParams(MessageType.Error, String.format("Unknown Command %s", command)));
+                    return null;
             }
-            else {
-                this.client.join().showMessage(new MessageParams(MessageType.Error, String.format("Unknown Command %s", command)));
-            }
-        }
-        catch (Exception e) {
-            return null;
-        }
-
-        return null;
 	}
 
     @Override
@@ -101,4 +92,106 @@ public class CqlWorkspaceService implements WorkspaceService {
             }
         }
     }
+
+
+    // There's currently not a "show text file" or similar command in the LSP spec,
+    // So it's not client agnostic. The client has to know that the result of this command
+    // is XML and display it accordingly.
+    private CompletableFuture<Object> viewXml(ExecuteCommandParams params) {
+        try {
+            String uriString = ((JsonElement)params.getArguments().get(0)).getAsString();
+            URI uri = new URI(uriString);
+            Optional<String> content = ((CqlTextDocumentService)this.server.getTextDocumentService()).activeContent(uri);
+            if (content.isPresent()) {
+                CqlTranslator translator = this.server.getTranslationManager().translate(uri, content.get());
+                return CompletableFuture.completedFuture(translator.toXml());
+            }
+
+            return null;
+        }
+        catch(Exception e) {
+            this.client.join().showMessage(new MessageParams(MessageType.Error, String.format("View XML failed with: %s", e.getMessage())));
+            return null;
+        }
+    }
+
+    // There's currently not a "show text file" or similar command in the LSP spec,
+    // So it's not client agnostic. The client has to know that the result of this command
+    // is text and display it accordingly.
+    private CompletableFuture<Object> executeCql(ExecuteCommandParams params) {
+        try {
+            String uriString = ((JsonElement)params.getArguments().get(0)).getAsString();
+            URI uri = new URI(uriString);
+            CqlTranslator translator = this.getTranslatorForUri(uri);
+
+            Library library = CqlLibraryReader.read(new ByteArrayInputStream(translator.toXml().getBytes()));
+
+
+            String path = null;
+            String endpoint = null;
+            String patientId = null;
+
+            if(params.getArguments().size() >= 2) {
+                path = ((JsonElement)params.getArguments().get(1)).getAsString();
+            }
+
+            if(params.getArguments().size() >= 3) {
+                endpoint = ((JsonElement)params.getArguments().get(2)).getAsString();
+            }
+
+            if(params.getArguments().size() >= 4) {
+                patientId = ((JsonElement)params.getArguments().get(3)).getAsString();
+            }
+
+            Context context = new Context(library);
+
+            FileBasedFhirProvider dataProvider = new FileBasedFhirProvider(path, endpoint);
+
+            context.registerDataProvider("http://hl7.org/fhir", dataProvider);
+
+
+            StringBuilder builder = new StringBuilder();
+
+            if (library.getStatements() != null) {
+                for (org.cqframework.cql.elm.execution.ExpressionDef def : library.getStatements().getDef()) {
+                    context.enterContext(def.getContext());
+                    if (patientId != null && !patientId.isEmpty()) {
+                        context.setContextValue(context.getCurrentContext(), patientId);
+                    }
+                    else {
+                        context.setContextValue(context.getCurrentContext(), "null");
+                    }
+    
+                    try {
+                        builder.append(def.getName() + " : ");
+                        Object res = def instanceof org.cqframework.cql.elm.execution.FunctionDef ? "Definition successfully validated" : def.getExpression().evaluate(context);
+    
+                        builder.append(res.toString() + "\n");
+                    }
+                    catch (RuntimeException re) {
+                        re.printStackTrace();
+    
+                        String message = re.getMessage() != null ? re.getMessage() : re.getClass().getName();
+                        builder.append(message + "\n");
+                    }
+                }
+            }
+
+            return CompletableFuture.completedFuture(builder.toString());
+        }
+        catch(Exception e) {
+            this.client.join().showMessage(new MessageParams(MessageType.Error, String.format("Execute CQL failed with: %s", e.getMessage())));
+            return null;
+        }
+    }
+
+    private CqlTranslator getTranslatorForUri(URI uri) {
+        Optional<String> content = ((CqlTextDocumentService)this.server.getTextDocumentService()).activeContent(uri);
+        if (content.isPresent()) {
+            return this.server.getTranslationManager().translate(uri, content.get());
+        }
+        
+        return null;
+    }
 }
+
