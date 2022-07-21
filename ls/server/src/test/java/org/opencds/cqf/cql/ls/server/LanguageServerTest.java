d985fa1c99a3c8b5f1d8ef65ec986dea449a54b5
Jonathan Percival <jonathan.i.percival@gmail.com>
2022-07-21 12:32:22 -0600

    Initial hover tests

--- ls/server/src/test/java/org/opencds/cqf/cql/ls/server/LanguageServerTest.java
+++ ls/server/src/test/java/org/opencds/cqf/cql/ls/server/LanguageServerTest.java
@@ -1,7 +1,11 @@
 package org.opencds.cqf.cql.ls.server;
 
+import static org.testng.Assert.assertEquals;
 import static org.testng.Assert.assertNotNull;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.TimeUnit;
 
 import org.eclipse.lsp4j.Hover;
@@ -9,8 +13,10 @@ import org.eclipse.lsp4j.HoverParams;
 import org.eclipse.lsp4j.InitializeParams;
 import org.eclipse.lsp4j.InitializeResult;
 import org.eclipse.lsp4j.InitializedParams;
+import org.eclipse.lsp4j.MarkupContent;
 import org.eclipse.lsp4j.Position;
 import org.eclipse.lsp4j.TextDocumentIdentifier;
+import org.eclipse.lsp4j.WorkspaceFolder;
 import org.eclipse.lsp4j.services.LanguageClient;
 import org.mockito.Mockito;
 import org.testng.annotations.Test;
@@ -45,20 +51,29 @@ public class LanguageServerTest {
         server.exited().get(100, TimeUnit.MILLISECONDS);
     }
 
-    // @Test
+    @Test
     public void hover() throws Exception {
         LanguageClient client = Mockito.mock(LanguageClient.class);
-        CqlLanguageServer server = new CqlLanguageServer();
+        ActiveContent content = new ActiveContent();
+        List<WorkspaceFolder> folders = new ArrayList<>();
+        CqlLanguageServer server = new CqlLanguageServer(new ServerContext(CompletableFuture.completedFuture(client), content, folders, new TestContentService()));
         server.connect(client);
         InitializeResult initializeResult = server.initialize(new InitializeParams()).get();
         assertNotNull(initializeResult);
         server.initialized(new InitializedParams());
 
-        Hover hover = server.getTextDocumentService().hover(new HoverParams(new TextDocumentIdentifier("Test.cql"), new Position(2, 5))).get();
+        Hover hover = server.getTextDocumentService().hover(new HoverParams(new TextDocumentIdentifier("file:/org/opencds/cqf/cql/ls/server/Two.cql"), new Position(5, 2))).get();
 
 
         server.shutdown().get(100, TimeUnit.MILLISECONDS);
         server.exit();
         server.exited().get(100, TimeUnit.MILLISECONDS);
+
+
+        assertNotNull(hover);
+        assertNotNull(hover.getContents().getRight());
+
+        MarkupContent markup = hover.getContents().getRight();
+        assertEquals(markup.getValue(), "```System.Integer```");
     }
 }
