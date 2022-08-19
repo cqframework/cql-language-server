73b1886cb8fdfe5cfba1bb0bb02daf7b3226e916
Jonathan Percival <jonathan.i.percival@gmail.com>
2022-08-19 14:21:38 -0600

    Tests and fixes for Uri parsing

--- core/src/main/java/org/opencds/cqf/cql/ls/core/utility/Uris.java
+++ core/src/main/java/org/opencds/cqf/cql/ls/core/utility/Uris.java
@@ -6,7 +6,7 @@ public class Uris {
     private Uris() {}
 
     public static URI getHead(URI uri) {
-        String path = uri.getPath();
+        String path = uri.getRawPath();
         if (path != null) {
             int index = path.lastIndexOf("/");
             if (index > -1) {
@@ -21,15 +21,16 @@ public class Uris {
 
     public static URI withPath(URI uri, String path) {
         try {
-            return new URI(uri.getScheme(), uri.getUserInfo(), uri.getHost(), uri.getPort(), path,
-                    uri.getFragment(), uri.getQuery());
+            return URI.create((uri.getScheme() != null ? uri.getScheme() + ":" : "") + "//"
+                    + createAuthority(uri.getRawAuthority()) + createPath(path)
+                    + createQuery(uri.getRawQuery()) + createFragment(uri.getRawFragment()));
         } catch (Exception e) {
             return null;
         }
     }
 
     public static URI addPath(URI uri, String path) {
-        return withPath(uri, uri.getPath() + path);
+        return withPath(uri, stripTrailingSlash(uri.getRawPath()) + createPath(path));
     }
 
     public static URI parseOrNull(String uriString) {
@@ -37,6 +38,44 @@ public class Uris {
             return new URI(uriString);
         } catch (Exception e) {
             return null;
+        }
+    }
+
+    private static String createAuthority(String rawAuthority) {
+        return rawAuthority != null ? rawAuthority : "";
+    }
+
+    private static String stripTrailingSlash(String path) {
+        if (path == null || path.isEmpty()) {
+            return "";
+        }
+
+        if (path.endsWith("/")) {
+            return path.substring(0, path.length() - 1);
+        }
+
+        return path;
+    }
+
+    private static String createPath(String pathValue) {
+        return ensurePrefix("/", pathValue);
+    }
+
+    private static String createQuery(String queryValue) {
+        return ensurePrefix("?", queryValue);
+    }
+
+    private static String createFragment(String fragmentValue) {
+        return ensurePrefix("#", fragmentValue);
+    }
+
+    private static String ensurePrefix(String prefix, String value) {
+        if (value == null || value.isEmpty()) {
+            return "";
+        } else if (value.startsWith(prefix)) {
+            return value;
+        } else {
+            return prefix + value;
         }
     }
 
---
+++ core/src/test/java/org/opencds/cqf/cql/ls/core/utility/UrisTest.java
@@ -0,0 +1,79 @@
+package org.opencds.cqf.cql.ls.core.utility;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import java.net.URI;
+import org.junit.jupiter.api.Test;
+
+public class UrisTest {
+
+    @Test
+    public void encodedUriGetHead() {
+        URI uri = URI.create("file:///d%3A/src/test.cql");
+        URI root = Uris.getHead(uri);
+        assertEquals("file:///d%3A/src", root.toString());
+
+
+        assertEquals("test.cql", root.relativize(uri).toString());
+    }
+
+    @Test
+    public void unencodedUriGetHead() {
+        URI uri = URI.create("file:///home/src/test.cql");
+        URI root = Uris.getHead(uri);
+        assertEquals("file:///home/src", root.toString());
+        assertEquals("test.cql", root.relativize(uri).toString());
+
+
+        uri = URI.create("https://hl7.org/fhir/test.cql");
+        root = Uris.getHead(uri);
+        assertEquals("https://hl7.org/fhir", root.toString());
+        assertEquals("test.cql", root.relativize(uri).toString());
+    }
+
+    @Test
+    public void uriAddPath() {
+        // With trailing slash
+        URI root = URI.create("file:///d%3A/src/");
+        URI uri = Uris.addPath(root, "test.cql");
+        assertEquals("file:///d%3A/src/test.cql", uri.toString());
+
+        uri = Uris.addPath(root, "/test.cql");
+        assertEquals("file:///d%3A/src/test.cql", uri.toString());
+
+        // Unencoded
+        root = URI.create("file:///home/src");
+        uri = Uris.addPath(root, "test.cql");
+        assertEquals("file:///home/src/test.cql", uri.toString());
+
+        // With Authority
+        root = URI.create("http://localhost:8080/home/src");
+        uri = Uris.addPath(root, "test.cql");
+        assertEquals("http://localhost:8080/home/src/test.cql", uri.toString());
+    }
+
+    @Test
+    public void uriWithPath() {
+        URI initial = URI.create("file:///d%3A/src/");
+        URI uri = Uris.withPath(initial, "/home/src/test.cql");
+        assertEquals("file:///home/src/test.cql", uri.toString());
+
+        uri = Uris.withPath(initial, "home/src/test.cql");
+        assertEquals("file:///home/src/test.cql", uri.toString());
+
+        // With authority
+        initial = URI.create("https://hl7.org/fhir");
+        uri = Uris.withPath(initial, "/fhir/test.cql");
+        assertEquals("https://hl7.org/fhir/test.cql", uri.toString());
+
+        uri = Uris.withPath(initial, "fhir/test.cql");
+        assertEquals("https://hl7.org/fhir/test.cql", uri.toString());
+
+        // With trailing slash on URI
+        initial = URI.create("http://locahost:8080/fhir/");
+        uri = Uris.withPath(initial, "/fhir/test.cql");
+        assertEquals("http://locahost:8080/fhir/test.cql", uri.toString());
+
+        uri = Uris.withPath(initial, "fhir/test.cql");
+        assertEquals("http://locahost:8080/fhir/test.cql", uri.toString());
+    }
+}
