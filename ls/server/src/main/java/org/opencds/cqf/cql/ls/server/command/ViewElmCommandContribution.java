415738b26ef51a1451a4ae37d684428bead1102b
raleigh-g-thompson <raleigh.g.thompson@gmail.com>
2026-02-10 13:04:30 -0500

    bump version to 4.1.2-SNAPSHOT adds support for View ELM and JSON

--- ls/server/src/main/java/org/opencds/cqf/cql/ls/server/command/ViewElmCommandContribution.java
+++ ls/server/src/main/java/org/opencds/cqf/cql/ls/server/command/ViewElmCommandContribution.java
@@ -4,9 +4,11 @@ import com.google.gson.JsonElement;
 import java.io.IOException;
 import java.net.URI;
 import java.util.Collections;
+import java.util.List;
 import java.util.Set;
 import java.util.concurrent.CompletableFuture;
 import org.cqframework.cql.cql2elm.CqlCompiler;
+import org.cqframework.cql.elm.serializing.ElmJsonLibraryWriter;
 import org.cqframework.cql.elm.serializing.ElmXmlLibraryWriter;
 import org.eclipse.lsp4j.ExecuteCommandParams;
 import org.hl7.elm.r1.Library;
@@ -43,23 +45,48 @@ public class ViewElmCommandContribution implements CommandContribution {
     // command
     // is XML and display it accordingly.
     private CompletableFuture<Object> viewElm(ExecuteCommandParams params) {
-        String uriString = ((JsonElement) params.getArguments().get(0)).getAsString();
-        try {
+        List<Object> args = params.getArguments();
 
+        // Defensive check: ensure we have at least the URI
+        if (args == null || args.isEmpty()) {
+            return CompletableFuture.completedFuture(null);
+        }
+
+        String uriString = ((JsonElement) args.get(0)).getAsString();
+
+        // Handle missing or null elmType by defaulting to "xml"
+        String elmType = "xml";
+        if (args.size() > 1 && args.get(1) != null) {
+            elmType = ((JsonElement) args.get(1)).getAsString();
+        }
+
+        try {
             URI uri = Uris.parseOrNull(uriString);
             CqlCompiler compiler = this.cqlCompilationManager.compile(uri);
+
             if (compiler != null) {
-                return CompletableFuture.completedFuture(convertToXml(compiler.getLibrary()));
+                // Use .equalsIgnoreCase for better robustness
+                if ("xml".equalsIgnoreCase(elmType)) {
+                    return CompletableFuture.completedFuture(convertToXml(compiler.getLibrary()));
+                } else {
+                    return CompletableFuture.completedFuture(convertToJson(compiler.getLibrary()));
+                }
             }
 
             return CompletableFuture.completedFuture(null);
         } catch (Exception e) {
+            // Log the error here if possible to avoid "silent" failures
             return CompletableFuture.completedFuture(null);
         }
     }
 
     private static String convertToXml(Library library) throws IOException {
         ElmXmlLibraryWriter writer = new ElmXmlLibraryWriter();
+        return writer.writeAsString(library);
+    }
+
+    private static String convertToJson(Library library) throws IOException {
+        ElmJsonLibraryWriter writer = new ElmJsonLibraryWriter();
         return writer.writeAsString(library);
     }
 }
